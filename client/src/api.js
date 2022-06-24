b76ec2a44e5e5f4e6a970e95293f6a74f9712b2c
Rizky Adi Pratama <71449366+rzkadp@users.noreply.github.com>
2022-06-24 15:38:21 +0700

    update

--- client/src/api.js
+++ client/src/api.js
@@ -1,34 +1,58 @@
 import axios from "axios";
-const url = "/api/post";
+const posturl = "/api/post";
+const userurl = "/api/user";
 
 export default class API {
     // get all post from server
     static async getAllPost(){
-        const res = await axios.get(url);
+        const res = await axios.get(posturl);
         return res.data;
     }
-
     // get single post by id
     static async getPostByID(id){
-        const res = await axios.get(`${url}/${id}`);
+        const res = await axios.get(`${posturl}/${id}`);
         return res.data;
     }
-
     // insert post into database
     static async addPost(post){
-        const res = await axios.post(url, post);
+        const res = await axios.post(posturl, post);
+        return res.data;
+    }
+    // update post
+    static async getPostByID(id, post){
+        const res = await axios.patch(`${posturl}/${id}`, post);
+        return res.data;
+    }
+    // delete post
+    static async deletePost(id){
+        const res = await axios.delete(`${posturl}/${id}`);
         return res.data;
     }
 
-    // update post
-    static async getPostByID(id, post){
-        const res = await axios.patch(`${url}/${id}`, post);
+    
+    // get all user from server
+    static async getAllUser(){
+        const res = await axios.get(userurl);
         return res.data;
     }
-    
-    // delete post
-    static async deletePost(id){
-        const res = await axios.delete(`${url}/${id}`);
+    // get single user by id
+    static async getUserByID(id){
+        const res = await axios.get(`${userurl}/${id}`);
+        return res.data;
+    }
+    // insert user into database
+    static async addUser(post){
+        const res = await axios.user(userurl, post);
+        return res.data;
+    }
+    // update user
+    static async getUserByID(id, post){
+        const res = await axios.patch(`${userurl}/${id}`, post);
+        return res.data;
+    }
+    // delete user
+    static async deleteUser(id){
+        const res = await axios.delete(`${userurl}/${id}`);
         return res.data;
     }
 }
--- client/src/views/AddPost.vue
+++ client/src/views/AddPost.vue
@@ -1,3 +1,12 @@
 <template>
-<h1>Add Post Page</h1>
+  <v-container>
+    <div class="col-md-3">
+        <v-text-field label="Main input" :rules="rules" hide-details="auto"></v-text-field>
+        <v-text-field label="Another input"></v-text-field>
+        <v-btn elevation="2" depressed color="primary">Save</v-btn>
+    </div>
+  </v-container>
 </template>
+
+<script>
+</script>
