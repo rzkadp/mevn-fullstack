9971f5128bd491272892142fc7df05ef8d184953
Rizky Adi Pratama <71449366+rzkadp@users.noreply.github.com>
2022-06-24 10:28:51 +0700

    create client front end

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,30 +1,73 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <v-app id="inspire">
+    <v-navigation-drawer
+      v-model="drawer"
+      app
+    >
+    <v-list-item>
+      <v-list-item-content>
+        <v-list-item-title>
+          Example App
+        </v-list-item-title>
+        <v-list-item-subtitle>
+          MEVN Full Stack
+        </v-list-item-subtitle>
+      </v-list-item-content>
+    </v-list-item>
+    <v-divider></v-divider>
+    <v-list>
+      <v-list-item-group v-model="model">
+        <v-list-item
+          v-for="(item, i) in items"
+          :key="i"
+          :to="item.link"
+        >
+          <v-list-item-icon>
+            <v-icon v-text="item.icon"></v-icon>
+          </v-list-item-icon>
+          <v-list-item-content>
+            <v-list-item-title v-text="item.title"></v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list-item-group>
+    </v-list>
+    </v-navigation-drawer>
+
+    <v-app-bar app>
+      <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+
+      <v-toolbar-title>Application</v-toolbar-title>
+    </v-app-bar>
+
+    <v-main>
+      <router-view>
+        
+      </router-view>
+    </v-main>
+  </v-app>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
+<script>
+  export default {
+    data: () => ({
+      drawer: null,
+      items: [
+        {
+          icon: 'mdi-home',
+          title: 'Home',
+          link: '/'
+        },
+        {
+          icon: 'mdi-note-plus',
+          title: 'Add Post',
+          link: '/add-post'
+        },
+        {
+          icon: 'mdi-help-box',
+          title: 'About',
+          link: '/about'
+        },
+      ],
+      }),
+  }
+</script>
